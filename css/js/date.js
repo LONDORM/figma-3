13e39000de2d1ff603bc7796c4eabd215d8f94c2
LONDORM <145760859+LONDORM@users.noreply.github.com>
2023-09-29 21:06:03 +0500

    Figma-3

---
+++ css/js/date.js
@@ -0,0 +1,22 @@
+const date = document.querySelector('.date')
+
+
+
+
+
+function innerDate() {
+
+    let day = new Date().getDate().toString().padStart(2, 0);
+    let month = (new Date().getMonth() + 1).toString().padStart(2, 0);
+    let year = new Date().getFullYear().toString();
+    let hour = new Date().getHours().toString().padStart(2, 0)
+    let minut = new Date().getMinutes().toString().padStart(2, 0)
+    
+let todayDate = day + '.' + month + '.' + year + " " 
+    date.innerHTML = todayDate;
+}
+innerDate()
+
+setInterval(() => {
+    innerDate()
+}, 1000)
